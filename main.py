dc82665b88843213fa619c923628b60c658d5ef4
Moussa-Kalam <moussakalamamzat@gmail.com>
2025-05-12 15:42:49 +0200

    feat: print report

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-from stats import count_words, count_chars
+from stats import count_words, count_chars, get_char_stats_sorted
 
 
 def main():
     book_path = "./books/frankenstein.txt"
     book_content = get_book_text(book_path)
-    print(book_content)
-
     num_words = count_words(book_content)
-    print(f"{num_words} words found in the document")
-
     num_chars = count_chars(book_content)
-    print(num_chars)
+    sorted_chars_stats_list = get_char_stats_sorted(num_chars)
+    print_report(book_path, num_words, sorted_chars_stats_list)
+
+
+def print_report(book_path, num_words, chars_stats_sorted):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing the book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for entry in chars_stats_sorted:
+        print(f"{entry['char']}: {entry["num"]}")
+    print("============= END ===============")
+
+
+
 
 
 def get_book_text(file_path):
--- stats.py
+++ stats.py
@@ -1,6 +1,7 @@
 def count_words(text):
     return len(text.split())
 
+
 def count_chars(text):
     chars_stats = {}
     for character in text:
@@ -10,3 +11,16 @@ def count_chars(text):
         else:
             chars_stats[char] = 1
     return chars_stats
+
+
+def sort_on(stats):
+    return stats['num']
+
+
+def get_char_stats_sorted(chars_stats):
+    report = []
+    for char in chars_stats:
+        if char.isalpha():
+            report.append({"char": char, "num": chars_stats[char]})
+    report.sort(reverse=True, key=sort_on)
+    return report
