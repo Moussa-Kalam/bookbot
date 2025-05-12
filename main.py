b2446fd5e62b21af608b8f448d3dec2874a3e114
Moussa-Kalam <moussakalamamzat@gmail.com>
2025-05-12 13:47:43 +0200

    feat: count the number of words in the book

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ def main():
     book_content = get_book_text(book_path)
     print(book_content)
 
+    num_words = count_words(book_content)
+    print(f"{num_words} words found in the document")
+
 
 def get_book_text(file_path):
     with open(file_path) as file:
         file_contents = file.read()
         return file_contents
+
+
+def count_words(text):
+    return len(text.split())
 
 
 main()
