a13b83eb399bf38ef5c144fc7d6eb29310993edb
Moussa-Kalam <moussakalamamzat@gmail.com>
2025-05-12 13:50:35 +0200

    refactor: move the count_words function to its own file

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from stats import count_words
+
+
 def main():
     book_path = "./books/frankenstein.txt"
     book_content = get_book_text(book_path)
@@ -11,10 +14,6 @@ def get_book_text(file_path):
     with open(file_path) as file:
         file_contents = file.read()
         return file_contents
-
-
-def count_words(text):
-    return len(text.split())
 
 
 main()
